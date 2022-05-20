8651e8c323d7c1b1a2a3f2ff27762ec39a035997
naru200 <jinsu2504@gmail.com>
2022-05-20 12:11:01 +0900

    Add feature: Upload slack message image to S3

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { AwsLambdaReceiver, App, subtype } from "@slack/bolt";
+import got from "got";
+import { S3 } from "aws-sdk";
+
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: awsLambdaReceiver,
+});
+
+const s3 = new S3();
+
+app.message(subtype("file_share"), async ({ event, say, client }) => {
+  if ("files" in event) {
+    await Promise.all(
+      event.files.map(async ({ url_private, name, mimetype }) => {
+        const image = await got(url_private, {
+          headers: { Authorization: process.env.SLACK_BOT_TOKEN },
+          responseType: "buffer",
+        }).buffer();
+
+        await s3
+          .upload({
+            Bucket: "slackbot-images",
+            Key: name,
+            ContentType: mimetype,
+            Body: image,
+          })
+          .promise();
+      }),
+    );
+
+    await client.reactions.add({
+      name: "white_check_mark",
+      timestamp: event.event_ts,
+      channel: event.channel,
+    });
+  }
+});
+
+export const handler = async (event, context, callback) =>
+  (await awsLambdaReceiver.start())(event, context, callback);
